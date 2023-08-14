96d1624f441de63719e3a9f267f987adb3219b43
yechen-idm <yechen@idmod.org>
2023-08-14 10:04:21 -0700

    update download script to look for image file created by post process script in historical simulations

--- workflow/download_output_pycomps.py
+++ workflow/download_output_pycomps.py
@@ -12,9 +12,10 @@ from COMPS.Data import Experiment
 
 
 class MyThread(threading.Thread):
-    def __init__(self, paths, output_path, sims_):
+    def __init__(self, paths, optional_paths, output_path, sims_):
         self.sims = sims_
         self.paths = paths
+        self.optional_paths = optional_paths
         self.output_path = output_path
         threading.Thread.__init__(self)
 
@@ -23,14 +24,26 @@ class MyThread(threading.Thread):
             start = time.time()
 
             asset_byte_arrays = sim.retrieve_output_files(paths=self.paths)
+            optional_asset_byte_arrays = []
+            found_optional_paths = []
 
+            for path in self.optional_paths:
+                try:
+                    optional_asset_byte_arrays.append(sim.retrieve_output_files(paths=[path])[0])
+                    found_optional_paths.append(path)
+                except RuntimeError as e:
+                    print(e)
+
+            p = os.path.join(self.output_path, 'exp_' + str(sim.experiment_id), 'sim_' + str(sim.id))
+            if not os.path.exists(p):
+                os.makedirs(p)
             for i in range(len(self.paths)):
-                p = os.path.join(self.output_path, 'exp_' + str(sim.experiment_id), 'sim_' + str(sim.id))
-                if not os.path.exists(p):
-                    os.makedirs(p)
-
                 with open(os.path.join(p, os.path.basename(self.paths[i])), 'wb') as outfile:
                     outfile.write(asset_byte_arrays[i])
+
+            for i in range(len(optional_asset_byte_arrays)):
+                with open(os.path.join(p, os.path.basename(found_optional_paths[i])), 'wb') as outfile:
+                    outfile.write(optional_asset_byte_arrays[i])
 
             print('| -> downloading/saving %d asset(s) took %.2f sec' % (len(self.paths), time.time() - start))
 
@@ -39,9 +52,9 @@ def download_output(exp_id_filepath: str, first_only: bool = False, output_path:
     """
     Download Emod simulation output csv files to output folder from Comps.
     Args:
-        exp_id_filepath (): expriment id filepath
-        first_only (): download the first simulation outputs only
-        output_path (): output file path
+        exp_id_filepath ():          experiment id filepath
+        first_only ():               download the first simulation outputs only
+        output_path ():              output file path
 
     Returns: list of results file paths
 
@@ -55,6 +68,7 @@ def download_output(exp_id_filepath: str, first_only: bool = False, output_path:
 
     paths = [os.path.join("output", manifest.infection_report),
              os.path.join("output", manifest.transmission_report)]
+    optional_paths = ['magude_inc.png', 'magude_prev.png', 'sac_prev.png', 'u5_prev.png']
 
     Client.login(manifest.compshost)
 
@@ -74,6 +88,12 @@ def download_output(exp_id_filepath: str, first_only: bool = False, output_path:
         p = os.path.join(output_path, 'exp_' + str(sim.experiment_id), 'sim_' + str(sim.id))
         for i in range(len(paths)):
             result_files.append(os.path.join(p, os.path.basename(paths[i])))
+        # if 'archetype' in sim.tags:
+        #     if 'magude_historical' == sim.tags['archetype']:
+        #         optional_paths.append('magude_inc.png')
+        #         optional_paths.append('magude_prev.png')
+        #     elif 'maka_historical' == sim.tags['archetype']:
+        #         optional_paths.append('sac_prev.png')
         if first_only:
             break
     if first_only:
@@ -86,7 +106,7 @@ def download_output(exp_id_filepath: str, first_only: bool = False, output_path:
     threads = []
 
     for i in range(manifest.download_num_threads):
-        t = MyThread(paths, output_path, all_sims[:sims_per_thread])
+        t = MyThread(paths, optional_paths, output_path, all_sims[:sims_per_thread])
         all_sims = all_sims[sims_per_thread:]
         threads.append(t)
 
@@ -111,6 +131,3 @@ if __name__ == '__main__':
                         default=manifest.output)
     args = parser.parse_args()
     download_output(args.exp_id_filepath, args.first_only, args.output_path)
-
-
-
