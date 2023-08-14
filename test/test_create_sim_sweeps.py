53909e24e3d7b2b29341fc55d22876f2165b5e89
yechen-idm <yechen@idmod.org>
2023-08-14 09:38:56 -0700

    remove non-unit stype tests

--- test/test_create_sim_sweeps.py
+++ test/test_create_sim_sweeps.py
@@ -1,75 +1,20 @@
 import shutil
 import unittest
-import os
+import pytest
 
-from run_sims import manifest
 from run_sims.create_sim_sweeps import create_and_run_sim_sweep
 from idmtools.entities.experiment import Experiment
 
-run_exp = False
+run_exp = True
 
 
 class TestCreateAndRunSimSweep(unittest.TestCase):
+    @pytest.mark.order(2)
     def test_default_values(self):
         exp = create_and_run_sim_sweep(run_exp=False)
         self.assertIsInstance(exp, Experiment)
 
-    def test_toy_archetype(self):
-        archetypes = ["test", "flat", "maka_like", "magude_like"]
-        pop_sizes = [1]
-        importations = [40]
-        target_prevalences = [0.05]
-        max_infections = [3]
-        number_of_seeds = 1
-
-        exp = create_and_run_sim_sweep(
-            archetypes=archetypes,
-            pop_sizes_in_thousands=pop_sizes,
-            importations_per_year_per_1000=importations,
-            target_prevalences=target_prevalences,
-            max_num_infections=max_infections,
-            number_of_seeds=number_of_seeds,
-            run_exp=run_exp
-        )
-        shutil.move(manifest.exp_id_file, os.path.join(manifest.output, 'toy_emod_exp.id'))
-
-        if run_exp:
-            self.assertTrue(exp.succeeded)
-        self.assertTrue(exp.simulation_count, 4)
-        archetypes = [x if x != 'test' else 'flat' for x in archetypes]
-        actual_archetypes = list()
-        for sim in exp.simulations:
-            actual_archetypes.append(sim.tags['archetype'])
-
-        self.assertListEqual(archetypes, actual_archetypes)
-
-    def test_historical_archetype(self):
-        archetypes = ["maka_historical", "magude_historical"]
-        pop_sizes = [1]
-        importations = [40]
-        max_infections = [3]
-        number_of_seeds = 1
-
-        exp = create_and_run_sim_sweep(
-            archetypes=archetypes,
-            pop_sizes_in_thousands=pop_sizes,
-            importations_per_year_per_1000=importations,
-            target_prevalences=None,
-            max_num_infections=max_infections,
-            number_of_seeds=number_of_seeds,
-            run_exp=run_exp
-        )
-        shutil.move(manifest.exp_id_file, os.path.join(manifest.output, 'historical_emod_exp.id'))
-
-        if run_exp:
-            self.assertTrue(exp.succeeded)
-        self.assertTrue(exp.simulation_count, 2)
-        actual_archetypes = list()
-        for sim in exp.simulations:
-            actual_archetypes.append(sim.tags['archetype'])
-
-        self.assertListEqual(archetypes, actual_archetypes)
-
+    @pytest.mark.order(2)
     def test_pop_size_sweep(self):
         archetypes = ["test"]
         pop_sizes = [1, 10, 20, 50, 100]
@@ -92,6 +37,7 @@ class TestCreateAndRunSimSweep(unittest.TestCase):
 
         self.assertSetEqual(set(pop_sizes), actual_pop_sizes)
 
+    @pytest.mark.order(2)
     def test_importation_sweep(self):
         archetypes = ["test"]
         pop_sizes = [20]
@@ -114,6 +60,7 @@ class TestCreateAndRunSimSweep(unittest.TestCase):
 
         self.assertSetEqual(set(importations), actual_importations)
 
+    @pytest.mark.order(2)
     def test_max_infections_sweep(self):
         archetypes = ["test"]
         pop_sizes = [50]
